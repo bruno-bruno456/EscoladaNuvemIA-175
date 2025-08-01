6856284acbe01e13b9d03f622efa198952c935f2
Bruno <bruno.marques@escoladanuvem.org>
2025-07-31 21:03:49 -0300

    Prática 05

---
+++ pratica05/palindromo.py
@@ -0,0 +1,26 @@
+"""
+Crie uma função que verifique se uma palavra ou frase é um palíndromo
+(lê-se igual de trás para frente, ignorando espaços e pontuação).
+
+Se o resultado é True, responda “Sim”, se o resultado for False, responda “Não”.
+"""
+
+
+
+def is_palindromo(texto):
+    texto_limpo = ''.join(char.lower()
+                          for char in texto
+                          if char.isalnum())
+
+    return texto_limpo == texto_limpo[::-1]
+
+
+frase = input("Forneça a frase ou expressão para verificação do palíndromo: ")
+resultado = is_palindromo(frase)
+
+if resultado == True:
+    resposta = "Sim"
+else:
+    resposta = "Não"    
+
+print(f"A frase '{frase}' é palíndromo? {resposta}.")    
